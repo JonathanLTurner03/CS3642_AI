9bcf2ddb9de164d311f1804ca38c27b86f4fee03
Jonathan Turner <turner@atlantissrv.com]>
2024-02-03 15:39:58 -0500

    Beginning DFS functionality.

--- src/Assignments/A1/solving_algorithms/DFS.java
+++ src/Assignments/A1/solving_algorithms/DFS.java
@@ -1,4 +1,21 @@
 package Assignments.A1.solving_algorithms;
 
+import Assignments.A1.models.Board;
+import Assignments.A1.models.Move;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+
 public class DFS {
+
+    private HashSet<Board> visited = new HashSet<>();
+    private HashMap<Board, Stack<Move>> moves = new HashMap<>();
+
+    public void dfs(Board currentState) {
+
+
+
+    }
+
 }
