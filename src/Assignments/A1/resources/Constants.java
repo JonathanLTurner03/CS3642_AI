945ef8fff6366ab64c63574f0ec2238755db11a6
Jonathan Turner <turner@atlantissrv.com]>
2024-02-03 12:46:34 -0500

    Created parameters to allow for changable ways of the algorithms conditions.

--- src/Assignments/A1/resources/Constants.java
+++
@@ -1,8 +0,0 @@
-package Assignments.A1.resources;
-
-public class Constants {
-
-    /* Used to prevent DFS from going down only 1 branch */
-    public static final int MAX_DEPTH = 100;
-
-}
---
+++ src/Assignments/A1/resources/Parameters.java
@@ -0,0 +1,14 @@
+package Assignments.A1.resources;
+
+/**
+ * Holds any constants needed. May be re-modified later to allow for configuration.
+ *
+ * @author Jonathan Turner
+ * @version Spring 2024
+ */
+public class Parameters {
+
+    /* Used to prevent DFS from going down only 1 branch */
+    public static final int MAX_DEPTH = 40; // Max number of moves in 8-Puzzle's are 31 moves if solvable.
+
+}
