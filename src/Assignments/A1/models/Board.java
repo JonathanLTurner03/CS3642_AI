a1e6bad17603b886e6c18cce0f062673c113c59d
Jonathan Turner <turner@atlantissrv.com]>
2024-02-02 20:10:22 -0500

    Added tests for Piece and added a valid move check.

---
+++ src/Assignments/A1/models/Board.java
@@ -0,0 +1,68 @@
+package Assignments.A1.models;
+
+/**
+ * This class keeps track of the current state (whether in permutation or not) of the board.
+ *
+ * @author Jonathan Turner
+ * @version Spring 2024
+ */
+public class Board {
+
+    private final Piece[] pieces;
+
+    /**
+     * Default constructor that generates the solved board.
+     *
+     * @precondition none
+     * @postcondition a solved board is created.
+     */
+    public Board() {
+        this.pieces = new Piece[9];
+        for (int index = 1; index <= 9; index++) {
+            this.pieces[index-1] = new Piece(index-1, index);
+        }
+    }
+
+    /**
+     * Constructor used to create a board with a pre-provided board.
+     *
+     * @precondition board.length == 9
+     * @postcondition a board is created with given locations
+     *
+     * @param board the provided state of the board.
+     */
+    public Board(Piece[] board) {
+        if (board.length != 9) {
+            throw new IllegalArgumentException("The board must be a array size of 9.");
+        }
+
+        this.pieces = board;
+    }
+
+
+    public boolean isTaken(int loc) {
+        if (invalidLocation(loc)) {
+            throw new IllegalArgumentException("Invalid location specified. Valid Range: 0 <= Location <= 8");
+        }
+        return (this.pieces[loc].getValue() != 0);
+    }
+
+    /**
+     * Returns the array of pieces.
+     *
+     * @precondition none
+     * @postcondition none
+     * @return the array of pieces.
+     */
+    public Piece[] getPieces() {
+        return this.pieces;
+    }
+
+
+    /* Private Methods Below. End of Java Docs. */
+
+    /* Checks if a given location is a valid location. */
+    private boolean invalidLocation(int loc) {
+        return (loc >= 0 && loc <= 8);
+    }
+}
