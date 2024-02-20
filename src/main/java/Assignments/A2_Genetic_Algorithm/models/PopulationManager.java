ef13b75dea6bc66111ae94c7dd07940dbeb1d44b
Jonathan Turner <turner@atlantissrv.com]>
2024-02-19 21:41:08 -0500

    Implemented CrossOver.

--- src/main/java/Assignments/A2_Genetic_Algorithm/models/PopulationManager.java
+++ src/main/java/Assignments/A2_Genetic_Algorithm/models/PopulationManager.java
@@ -1,5 +1,7 @@
 package Assignments.A2_Genetic_Algorithm.models;
 
+import java.util.Random;
+
 /**
  * A class to manage the population of individuals and performs
  * Genetic Algorithm operations and functions.
@@ -9,6 +11,7 @@ package Assignments.A2_Genetic_Algorithm.models;
  */
 public class PopulationManager {
 
+    // The population of individuals
     private Individual[] population;
 
     /**
@@ -20,15 +23,13 @@ public class PopulationManager {
      * @param n_pop the number of individuals in the population.
      */
     public PopulationManager(int n_pop) {
-        // Check if the population size is positive
+        /* Check if the population size is positive */
         if (n_pop < 1) {
             throw new IllegalArgumentException("Population size must be positive.");
         }
 
-        // Create a new population of individuals
+        /* Initialize and creates the population of n_pop individuals with random genes */
         this.population = new Individual[n_pop];
-
-        // Initialize the population with random genes
         for (int i = 0; i < n_pop; i++) {
             this.population[i] = new Individual();
         }
@@ -45,18 +46,56 @@ public class PopulationManager {
      * @return the fitness of the individual
      */
     private int fitness(Individual individual) {
-        // Holds the decimal value of the binary genes.
+        /* Calculate the decimal value of the binary genes */
         int decimal = 0;
-
-        // Calculate the decimal value of the binary genes
         for (int i = individual.genes.length-1; i >= 0; i--) {
             if (individual.genes[i] == 1) {
-                // Add the value of the gene to the decimal value based on its position
+                /* Add the value of the gene to the decimal value based on its position */
                 decimal += (int) Math.pow(2, individual.genes.length-1-i);
             }
         }
 
-        // Return the fitness of the individual, which is f(x) = x^2.
+        /* Return the fitness of the individual, which is f(x) = x^2. */
         return (int) Math.pow(decimal, 2);
+    }
+
+    /**
+     * Breeds the two parent individuals to two new child individuals.
+     *
+     * @precondition firstParent != null && secondParent != null
+     * @postcondition none
+     *
+     * @param firstParent the first parent individual
+     * @param secondParent the second parent individual
+     *
+     * @return an array of two new individuals that are the children of the parents.
+     */
+    private Individual[] crossover(Individual firstParent, Individual secondParent) {
+        /* Check if the parents are not null */
+        if (firstParent == null || secondParent == null) {
+            throw new IllegalArgumentException("Parents cannot be null.");
+        }
+
+        /* Create a new individual to hold the genes of the children */
+        Individual firstChild = new Individual();
+        Individual secondChild = new Individual();
+
+        /* Randomly select a crossover point */
+        Random generator = new Random();
+        int crossover_point = generator.nextInt(0, 6);
+
+        /* Copy the genes of the same side parents to the same side child */
+        for (int i = 0; i < crossover_point; i++) {
+            firstChild.genes[i] = firstParent.genes[i];
+            secondChild.genes[i] = secondParent.genes[i];
+        }
+
+        /* Copy the genes of the opposite side parents to the opposite side child */
+        for (int i = crossover_point; i < 6; i++) {
+            firstChild.genes[i] = secondParent.genes[i];
+            secondChild.genes[i] = firstParent.genes[i];
+        }
+
+        return new Individual[] {firstChild, secondChild};
     }
 }
