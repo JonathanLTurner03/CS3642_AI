ccf590903d640b0fc2386f4affa6ce5582170e7f
Jonathan Turner <turner@atlantissrv.com]>
2024-02-20 18:03:55 -0500

    Added support for Performance Analysis as a menu option.

--- src/main/java/Assignments/A2_Genetic_Algorithm/Driver.java
+++ src/main/java/Assignments/A2_Genetic_Algorithm/Driver.java
@@ -3,7 +3,6 @@ package Assignments.A2_Genetic_Algorithm;
 import Assignments.A2_Genetic_Algorithm.models.BinaryVector;
 import Assignments.A2_Genetic_Algorithm.models.PopulationManager;
 
-import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -32,7 +31,7 @@ public class Driver {
      */
     public void start() {
         int option = this.getOption();
-        while (option != 4) {
+        while (option != 5) {
             this.executeOption(option);
             option = this.getOption();
         }
@@ -86,6 +85,26 @@ public class Driver {
             long end = System.currentTimeMillis();
             System.out.println("The Genetic Algorithm (GA) proceeded through " + result + " generations.");
             System.out.println("The algorithm took " + (end-start) + " ms to find the BinaryVector with fitness 0.");
+        } else {
+            if (PopulationManager.n_pop == 0 || PopulationManager.p_m == 0.0) {
+                System.out.println("You must enter a population size and mutation frequency to run the performance analysis.");
+                System.out.println();
+                return;
+            }
+
+            long totalTime = 0;
+            int averageGenerations = 0;
+            for (int i = 0; i < 50; i++) {
+                long start = System.currentTimeMillis();
+                BinaryVector[] population = PopulationManager.generate_population(PopulationManager.n_pop);
+                averageGenerations += PopulationManager.evolution(population);
+                long end = System.currentTimeMillis();
+                totalTime += end-start;
+            }
+            System.out.println("---------RESULTS---------");
+            System.out.println("Configuration: n_pop: " + PopulationManager.n_pop + " | p_m: " + PopulationManager.p_m);
+            System.out.println("The Genetic Algorithm (GA) on average ran through " + (averageGenerations/50) + " generations to complete.");
+            System.out.println("The average runtime for the GA was: " + (totalTime/50.0) + "ms");
         }
         System.out.println();
     }
@@ -138,7 +157,8 @@ public class Driver {
         System.out.println("1. Set Population Size");
         System.out.println("2. Set Mutation Percentage");
         System.out.println("3. Perform Genetic Algorithm (GA)");
-        System.out.println("4. Exit program");
+        System.out.println("4. Perform Performance Analysis (50 runs)");
+        System.out.println("5. Exit program");
         System.out.println();
     }
 
@@ -157,7 +177,7 @@ public class Driver {
         // Asks for the option and begins the loop until a valid option is gathered.
         System.out.print("Enter option number: ");
         int input = getIntegerInput();
-        while (input < 1 || input > 4) { // Compares it to the valid options available.
+        while (input < 1 || input > 5) { // Compares it to the valid options available.
             System.out.println("\nPlease enter a valid input.");
             System.out.print("Enter option number: ");
             input = getIntegerInput();
