42dc7015f8d8e1c2d2570aa5cb5ff8cb1bc73c56
Jonathan Turner <turner@atlantissrv.com]>
2024-02-20 18:10:22 -0500

    Documented code.

--- src/main/java/Assignments/A2_Genetic_Algorithm/Driver.java
+++ src/main/java/Assignments/A2_Genetic_Algorithm/Driver.java
@@ -46,61 +46,75 @@ public class Driver {
      * @param option the Option being executed.
      */
     public void executeOption(int option) {
+        /* If the user selects the option "Set Population Size" */
         if (option == 1) {
+            /* Gets the initial user input for the size */
             System.out.println("Please enter a population size.");
             System.out.print("Size: ");
             int n_pop = this.getIntegerInput();
+
+            /* Loops while the size is not positive and continues to prompt */
             while (n_pop < 0) {
                 System.out.println("Please enter a valid population size. Must be greater than 0.");
                 System.out.print("Size: ");
                 n_pop = this.getIntegerInput();
             }
             PopulationManager.n_pop = n_pop;
-        } else if (option == 2) {
+        } else if (option == 2) { /* If the user selects the option "Set Mutation Frequency" */
             if (PopulationManager.n_pop == 0) {
                 System.out.println("Please enter a population size before mutation frequency.");
                 System.out.println();
                 return;
             }
-
+            /* Gets the initial user input for the frequency */
             System.out.println("Please enter a mutation frequency. (Ex. 17% = .17)");
             System.out.print("Frequency: ");
             double p_m = this.getDoubleInput();
+
+            /* Loops while the mutation frequency is not valid and continues to prompt */
             while (p_m <= 0 || p_m >= 1) {
                 System.out.println("Please enter a valid mutation frequency. (Ex. 17% = .17) (0 <= freq <= 1)");
                 System.out.print("Frequency: ");
                 p_m = this.getDoubleInput();
             }
             PopulationManager.p_m = p_m;
-        } else if (option == 3) {
+        } else if (option == 3) { /* If the user selects the option "Run GA" */
+            /* Checks if the user has specified the population and mutation settings */
             if (PopulationManager.n_pop == 0 || PopulationManager.p_m == 0.0) {
                 System.out.println("You must enter a population size and mutation frequency to run the GA.");
                 System.out.println();
                 return;
             }
 
+            /* Begins the performance of the algorithm. Uses System.currentTimeMillis() to track performance time. */
             long start = System.currentTimeMillis();
             BinaryVector[] population = PopulationManager.generate_population(PopulationManager.n_pop);
             int result = PopulationManager.evolution(population);
             long end = System.currentTimeMillis();
+
+            /* Prints out hte results */
             System.out.println("The Genetic Algorithm (GA) proceeded through " + result + " generations.");
             System.out.println("The algorithm took " + (end-start) + " ms to find the BinaryVector with fitness 0.");
-        } else {
+        } else { /* If the user selects the option "Run Performance Analysis" */
+            /* Checks if the user has specified the population and mutation settings */
             if (PopulationManager.n_pop == 0 || PopulationManager.p_m == 0.0) {
                 System.out.println("You must enter a population size and mutation frequency to run the performance analysis.");
                 System.out.println();
                 return;
             }
 
+            /* Begins the loop of running the algorithm 50 times to check performance */
             long totalTime = 0;
             int averageGenerations = 0;
             for (int i = 0; i < 50; i++) {
                 long start = System.currentTimeMillis();
                 BinaryVector[] population = PopulationManager.generate_population(PopulationManager.n_pop);
-                averageGenerations += PopulationManager.evolution(population);
+                averageGenerations += PopulationManager.evolution(population); // Adds the number of generations to the average.
                 long end = System.currentTimeMillis();
-                totalTime += end-start;
+                totalTime += end-start; // adds the time performance to the total time.
             }
+
+            /* Prints out the results of the performance analysis. Also shows the current configuration for easier referencing */
             System.out.println("---------RESULTS---------");
             System.out.println("Configuration: n_pop: " + PopulationManager.n_pop + " | p_m: " + PopulationManager.p_m);
             System.out.println("The Genetic Algorithm (GA) on average ran through " + (averageGenerations/50) + " generations to complete.");
@@ -155,7 +169,7 @@ public class Driver {
     private void displayMenu() {
         System.out.println("-----------------MAIN MENU-----------------");
         System.out.println("1. Set Population Size");
-        System.out.println("2. Set Mutation Percentage");
+        System.out.println("2. Set Mutation Frequency");
         System.out.println("3. Perform Genetic Algorithm (GA)");
         System.out.println("4. Perform Performance Analysis (50 runs)");
         System.out.println("5. Exit program");
